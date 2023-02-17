144bfdd6e4c6367a3f266873ec63a7b3be918fd2
michelxd456 <michelbalvinor@gmail.com>
2023-02-17 12:44:06 -0500

    Primer ejercicios

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,20 @@
 // this is your main.js script
+let desafio = '30 días de JavaScript';
+console.log(desafio);
+console.log(desafio.length);
+console.log(desafio.toUpperCase());
+console.log(desafio.toLowerCase());
+console.log(desafio.substring(0,2))
+console.log(desafio.substring(3,21))
+console.log(desafio.includes("Script"))
+console.log(desafio.split(' '));
+let empre = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+console.log(empre.split(','));
+console.log(desafio.replace('JavaScript','Python'));
+console.log(desafio.charAt(15));
+console.log(desafio.charCodeAt('J'));
+console.log(desafio.indexOf('a'));
+console.log(desafio.lastIndexOf('a'));
+console.log( 'No puede terminar una oración con porque porque porque es una conjunción'.indexOf('porque'));
+console.log('No puede terminar una oración con porque porque porque es una conjunción'.lastIndexOf('porque'));
+console.log('No puede terminar una oración con porque porque porque es una conjunción'.search('porque'))
